de8a95a66b824f42a3c2c07913981f9a7c8b1758
daniyal <daniprog01@gmail.com>
2025-12-17 09:59:08 +0500

    Initial commit: first Java program

---
+++ src/Maain.java
@@ -0,0 +1,13 @@
+package com.Daniyal.library;
+
+public class Maain {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Library Management System");
+        System.out.println("This project is designed to manage books and library members.");
+        System.out.println("Main features will include:");
+        System.out.println("- Adding and removing books");
+        System.out.println("- Registering members and tracking borrowed books");
+        System.out.println("Developed by: Daniyal | Object-Oriented Programming Course");
+    }
+}
