19831bc2b6373f41bc9d4225f4d96fc19aab0164
daniyal <daniprog01@gmail.com>
2025-12-17 10:03:14 +0500

    Fix main class name

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+package com.Daniyal.library;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Library Management System");
+        System.out.println("This project is designed to manage books and library members.");
+        System.out.println("Main features will include:");
+        System.out.println("- Adding and removing books");
+        System.out.println("- Registering members and tracking borrowed books");
+        System.out.println("Developed by: Daniyal | Object-Oriented Programming Course");
     }
 }
+
